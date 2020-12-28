0387c69214219fa3ad5b6602bfe9b50db0768ab0
aldoBangun <punk.aldo68@gmail.com>
2020-12-28 18:08:15 +0800

    add bookRoute middleware to app.js

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const bookRoute = require('./routes/books')
 
 app.set('view engine', 'pug')
 
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
+app.use('/', bookRoute)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, console.log(`App is running on http://localhost:${PORT}`))
