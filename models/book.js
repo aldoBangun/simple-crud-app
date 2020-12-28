fd77f96452c93e0acc2b5e070ce8e81daf6e823a
aldoBangun <punk.aldo68@gmail.com>
2020-12-28 17:43:43 +0800

    add book models

---
+++ models/book.js
@@ -0,0 +1,17 @@
+const sql = require('../config/db.config')
+
+module.exports = {
+   createBook: (data, callback) => {
+      sql.query('INSERT INTO book SET ?', data, callback)
+   },
+   getBooks: (callback) => {
+      sql.query('SELECT * FROM book JOIN author ON book.author_id = author.id', callback)
+   },
+   getBookByTitle: (keyword, callback) => {
+      sql.query(
+         'SELECT * FROM book JOIN author ON book.author_id = author.id WHERE title LIKE "%?%"',
+         keyword,
+         callback
+      )
+   }
+}
